0b21ad82b5ca6dede3707594bf47323835e32081
nefformat <redaktor@nefformat.ru>
2015-04-03 09:50:36 +1000

    Add starter-template and some trash

--- app.js
+++ app.js
@@ -14,8 +14,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+if (app.get('env') === 'development') {
+  app.disable('view cache');
+  app.locals.pretty = true;
+}
+
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
+'use strict';
+/* GET bootstrap template */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+    //res.send('there will be bootstrap template');
+});
+
+router.get('/template/:selectedTemplate', function (req, res) {
+    res.render('bootstrap3-templates/' + req.params.selectedTemplate, {
+        'pathToAssets': '/bootstrap-3.3.1',
+        'pathToSelectedTemplateWithinBootstrap' : '/bootstrap-3.3.1/docs/examples/' + req.params.selectedTemplate
+    });
+});
+
+router.get('/about', function (req, res) {
+    res.render('about', {
+        'pathToAssets': '/bootstrap-3.3.1',
+        'pathToSelectedTemplateWithinBootstrap' : '/about'
+    });
 });
 
 module.exports = router;
+
